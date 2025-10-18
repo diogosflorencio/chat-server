63c4379bcc53dec4d8e14dbef1fdc45f79b63ff1
diogosflorencio <diogosflorencio@gmail.com>
2025-10-18 17:58:47 -0300

    Adição de replicação de mensagem no servidor

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ wss.on('connection', function connection(ws){
     ws.on('error', console.error);
 
     ws.on('message', function message(dados){
-        console.log(`recebido: %s`, dados);
+        console.log(`o servidor recebeu essa mensagem: %s`, dados);
+        ws.send(dados)
+        console.log("o servidor repasou a msg")
     });
-    ws.send('algo');
+    ws.send('Iniciando o servidor. ChatON!');
 })
