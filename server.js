b05475f8905ed8b1c8be4e7cb32d343df807961d
diogosflorencio <diogosflorencio@gmail.com>
2025-10-18 18:11:18 -0300

    reversão

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const wss = new WebSocketServer({port:8080});
 wss.on('connection', function connection(ws){
     ws.on('error', console.error);
 
+    
+    ws.send('Iniciando o servidor. ChatON!');
+
     ws.on('message', function message(dados){
         console.log(`o servidor recebeu essa mensagem: %s`, dados);
-        ws.send(dados)
+        ws.send(dados);
         console.log("o servidor repasou a msg")
     });
-    ws.send('Iniciando o servidor. ChatON!');
 })
