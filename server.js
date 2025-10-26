75ccf81d18174a05fed1f60b0adea9fa8667eeba
diogosflorencio <diogosflorencio@gmail.com>
2025-10-26 17:14:04 -0300

    ajuste na formatação do código e melhorias na lógica de envio de mensagens, incluindo a exclusão de mensagens do servidor e a atualização do histórico

--- server.js
+++ server.js
@@ -1,44 +1,45 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({port:8080});
+const wss = new WebSocketServer({ port: 8080 });
 
 let mensagensHistorico = []
-wss.on('connection', function connection(ws){
+wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
     // envia historico de mensagem pra quem se conecta 
-    
+
     const historico = {
         tipo: "historico",
         mensagens: mensagensHistorico
     };
-        
-    
-    ws.on('message', function message(dados){
+
+
+    ws.on('message', function message(dados) {
 
         console.log(`o servidor recebeu essa mensagem: %s`, dados);
         // implemetação de ping pong padrão do websocket pra manter a conexão dos clientes com o servidor.
-        if(dados=="ping") {
+        if (dados == "ping") {
             return;
         }
 
-        if(dados.toString().endsWith("/limpar")){
+        if (dados.toString().endsWith("/limpar")) {
             mensagensHistorico.length = 0
             return;
         }
-        
-        mensagensHistorico.push(dados.toString())
-        
-        console.log("mensagens no historico: " + mensagensHistorico)
-        
         wss.clients.forEach(cleinte => {
-            if(cleinte.readyState===WebSocket.OPEN){ // verifico se a conexão do cliente ainda é valida, assim não envio pra absolutamente todos, só pros ativos
+            if (cleinte.readyState === WebSocket.OPEN) { // verifico se a conexão do cliente ainda é valida, assim não envio pra absolutamente todos, só pros ativos
                 cleinte.send(dados.toString());
+
+                // para que a mensagem de "entrou no server não se matenha no historico". o ideial seria que a mensagem tivesse um tipo, tipo "msg do server" assim, não teria problema se o usuario digitasse exatamente essa
+                if (dados.toString().includes("entrou na sala!")) return;
+                mensagensHistorico.push(dados.toString())
             }
         });
-        
+
+        console.log("mensagens no historico: " + mensagensHistorico)
+
         console.log("o servidor repasou a msg")
-        
+
     });
     ws.send(JSON.stringify(historico))
 })
